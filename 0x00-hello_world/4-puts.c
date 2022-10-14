4c808a4993a7a70905290a8f4af3bbff900a3d38
Rexford Koomson <rexford.kk@gmail.com>
2022-10-14 06:21:04 -0700

    Initial commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Start point
+ *
+ * Description: Display 'Programming is like building a multilingual puzzle'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
